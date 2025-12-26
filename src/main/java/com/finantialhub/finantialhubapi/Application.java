ae750985422b575961013a4aaa06c8306a1e94ad
Emiliano Somoza <emisomoza@hotmail.com>
2025-12-26 17:34:57 +1000

    Delete empty tests

--- src/main/java/com/finantialhub/finantialhubapi/Application.java
+++ src/main/java/com/finantialhub/finantialhubapi/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FinantialhubapiApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FinantialhubapiApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
